6cb8785d8276ac19bb13447d0781f70a4b3d04e2
Albert Lionelle <albert.lionelle@colostate.edu>
2023-01-25 21:04:11 -0700

    added initial c templates

---
+++ sorts.h
@@ -0,0 +1,93 @@
+#ifndef SORTS_H
+#define SORTS_H
+
+#include <stdlib.h>
+#include "sort_helper.h"
+
+/*** code for selection sort ****/
+
+// Returns the minimum integer from a range in an array
+// Input: array - An array of integers
+//        start - Where to start looking in an array
+//        stop - End of where to search. Typically the 'size' of the array.
+// Output: The index in an array of the minimum value between a range [start,stop]
+int findMinimum(int *array, int start, int stop)
+{
+    return 0; // modify to return the index of the min value
+}
+
+
+// =============== Sort Function ===============
+// Provided below is a sort function. I have also
+// provided a template for how to document functions
+// to help organize your code.
+// Name: sort
+// Input(s):
+//  - 'array' is a pointer to an integer address.
+//     This is the start of some 'contiguous block of memory' that we will sort.
+//  - 'size' tells us how big the array of data is we are sorting.
+// Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
+void selectionSortIntegers(int *array, unsigned int size)
+{
+    // todo: implement selection sort
+}
+
+/***  Code for Insertion Sort ***/
+
+// =============== Sort Function ===============
+// Provided below is a sort function. I have also
+// provided a template for how to document functions
+// to help organize your code.
+// Name: sort
+// Input(s):
+//  - 'array' is a pointer to an integer address.
+//     This is the start of some 'contiguous block of memory' that we will sort.
+//  - 'size' tells us how big the array of data is we are sorting.
+// Output: No value is returned, but 'array' should be modified to store a sorted array of numbers.
+void insertionSortIntegers(int *array, unsigned int size)
+{
+    // TODO: Implement insertion sort
+ 
+
+}
+
+/** Code for Bubble Sort (from Lab -if not compiling, comment out the internals, but leave the function definition) ***/
+
+// =============== Sort Function ===============
+// Name: bubblesort
+// Desc: O(n^2) comparison sort
+// param(1): 'array' is a pointer to an integer address.
+//           This is the start of some 'contiguous block of memory'
+//           that we will sort.
+// param(2)  'size' tells us how big the array of
+//           data is we are sorting.
+// Output:   No value is returned, but 'array' should
+//           be modified to store a sorted array of size.
+void bubbleSortIntegers(int *array, unsigned int size)
+{
+    // code generated from lab
+
+}
+
+
+// provided code 
+
+// =============== Helper Functions ===============
+// Name:    compare
+// Desc:    A compare function which returns
+//          a value (positive,negative, or 0)
+//          to show if the result is >,<, or =.
+//
+// Input:   a and b here are generic types,
+//          that is why they are 'void'
+int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+void quickSortIntegers(int* array, unsigned int size) 
+{
+    qsort(array, 11, sizeof(int), compare);
+}
+
+#endif
